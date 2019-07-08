f1f07fc5c70a0d3362204790c5b8f7625c23b5ef
Lucas D <l.dupias@net.estia.fr>
2019-07-08 14:15:14 +0200

    Fixed camera component

--- camera/MyCamera.js
+++ camera/MyCamera.js
@@ -9,21 +9,48 @@ export class MyCamera extends Component {
         return (
             <View style={styles.container}>
                 <Text>oELO</Text>
+                <RNCamera
+                    style={styles.preview}
+                    type={RNCamera.Constants.Type.back}
+                    flashMode={RNCamera.Constants.FlashMode.on}
+                    androidCameraPermissionOptions={{
+                        title: 'Permission to use camera',
+                        message: 'We need your permission to use your camera',
+                        buttonPositive: 'Ok',
+                        buttonNegative: 'Cancel',
+                    }}
+                    androidRecordAudioPermissionOptions={{
+                        title: 'Permission to use audio recording',
+                        message: 'We need your permission to use your audio',
+                        buttonPositive: 'Ok',
+                        buttonNegative: 'Cancel',
+                    }}
+                >
+                    {({ camera, status, recordAudioPermissionStatus }) => {
 
+                        return (
+                            <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                                <TouchableOpacity onPress={() => this.takePicture(camera)} style={styles.capture}>
+                                    <Text style={{ fontSize: 14 }}> SNAP </Text>
+                                </TouchableOpacity>
+                            </View>
+                        );
+                    }}
+                </RNCamera>
             </View>
         );
     }
 
-    takePicture = async function() {
+    takePicture = async function () {
         if (this.camera) {
-            const options = {width: 50, quality: 0.5, base64: true };
+            const options = { width: 50, quality: 0.5, base64: true };
             this.setState({
                 loading: true
             });
             try {
                 const data = await this.camera.takePictureAsync(options);
                 console.log(data.uri);
-            }catch (e) {
+            } catch (e) {
                 console.log(e)
             }
         }
@@ -33,13 +60,13 @@ export class MyCamera extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'column',
-        backgroundColor: 'black',
+
     },
     preview: {
         flex: 1,
         justifyContent: 'flex-end',
         alignItems: 'center',
+        height: 500
     },
     capture: {
         flex: 0,
