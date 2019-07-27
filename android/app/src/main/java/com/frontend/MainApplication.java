093f4565d616efe735d8d18a5fc83ad3b22c05d1
ronn <ronnie-filth@hotmail.com>
2019-07-27 18:04:46 +0200

    Remove base 64 component

--- android/app/src/main/java/com/frontend/MainApplication.java
+++ android/app/src/main/java/com/frontend/MainApplication.java
@@ -3,7 +3,6 @@ package com.frontend;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import fr.snapp.imagebase64.RNImgToBase64Package;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNImgToBase64Package(), new RNCameraPackage()
+              new RNCameraPackage()
       );
     }
 
