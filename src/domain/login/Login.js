e93e3903d9e100eef8810ec2998d3098473b6fa1
ronn <ronnie@nextdigital.es>
2019-08-11 00:59:30 +0200

    login advance and refactor

--- src/domain/login/Login.js
+++ src/domain/login/Login.js
@@ -1,48 +1,59 @@
-import React, { Component } from 'react';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
 
 import {Logo} from './Logo';
 import {Form} from './Form';
+import {SignUpButton} from "./SignUpButton";
+import base64 from 'react-native-base64'
+import {executeBasicAuthenticationService} from "../service/AuthenticationService";
 
 
 export class LoginScreen extends Component {
 
-    static navigationOptions = {
-        header: null
+    constructor(props){
+        super(props)
+        this.state = {
+            userName: '',
+            pass: '',
+            hasLoginFailed: false,
+            showSuccessMessage: false
+        }
     }
 
     render() {
-        const {navigate} = this.props.navigation;
         return (
-            <View style={styles.container}>
+            <View style={styles1.container}>
                 <Logo />
-                <Form type="Login" navigation={this.props.navigation}/>
-                <View style={styles.signupTextCont}>
-                    <Text>Don't have an account?</Text>
-                    <TouchableOpacity onPress={() => navigate('Signin')}>
-                        <Text style={styles.signupText}> Signup</Text>
-                    </TouchableOpacity>
-                </View>
+                <Form
+                    type="Login"
+                    navigation={this.props.navigation}
+                    loginClicked={this.loginClicked.bind(this)}
+                    userName={this.state.userName}
+                    pass={this.state.pass}
+                    readUserName={this.readUserName.bind(this)}
+                    readPass={this.readPass.bind(this)}
+                />
+                {this.state.hasLoginFailed && <Text>Invalid Credentials</Text>}
+                {this.state.showSuccessMessage && <Text>Login Sucessful</Text>}
+                <SignUpButton navigation={this.props.navigation}/>
             </View>
         );
     }
+
+    readUserName = userName => this.setState({userName})
+
+    readPass = pass => this.setState({pass})
+
+    loginClicked = () => () => {
+        executeBasicAuthenticationService(this.state.userName, this.state.pass)
+    }
 }
-const styles = StyleSheet.create({
+
+const styles1 = StyleSheet.create({
     container: {
         backgroundColor: "#e0e0e0",
         flexGrow: 1,
         alignItems: 'center',
         justifyContent: 'center',
     },
-    signupTextCont: {
-        flexGrow: 1,
-        alignItems: 'center',
-        flexDirection: 'row',
-        paddingVertical: 16,
-        justifyContent: 'center',
-    },
-    signupText: {
-        fontSize: 16,
-        fontWeight: "500"
-    }
 })
