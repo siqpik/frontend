43a1c485e9ee41f260cfb3c80ba76308f1e491af
ronn <ronnie@nextdigital.es>
2019-08-11 17:31:34 +0200

    merge with dev

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ const App = createStackNavigator(
         Home: HomeScreen,
         Alert: AlertBeforePic,
         Profile: Profile,
-        Login: LoginScreen
+        Login: LoginScreen,
         TakeNewPic: TakeNewPic
     },
     {
