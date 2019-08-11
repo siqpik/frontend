911d338e1d7122c9f77a86214e7b79ab6f2e9063
ronn <ronnie@nextdigital.es>
2019-08-11 17:08:49 +0200

    Add basicAuth

--- android/app/src/main/java/com/frontend/MainApplication.java
+++ android/app/src/main/java/com/frontend/MainApplication.java
@@ -3,6 +3,10 @@ package com.frontend;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import org.reactnative.camera.RNCameraPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.facebook.react.ReactNativeHost;
@@ -27,9 +31,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
+            new RNCameraPackage(),
             new RNGestureHandlerPackage(),
-            new ReanimatedPackage(),
-              new RNCameraPackage()
+            new ReanimatedPackage()
       );
     }
 
