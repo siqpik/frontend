c75cc8cf6aa521bf9ad167bd8d449d0ebd9db5cb
ronn <ronnie@nextdigital.es>
2019-09-28 20:01:33 +0200

    Ad feature tu get Posts

--- src/domain/home/HomeScreen.js
+++ src/domain/home/HomeScreen.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import {ScrollView, Text} from "react-native";
+import axios from "axios";
+import Post from "../navigation/model/Post";
 
 export class HomeScreen extends React.Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            posts: []
+        }
+    }
+
+    componentDidMount(): void {
+        this.getPosts()
+    }
+
     render() {
         return (
             <ScrollView>
-                <Text >Aquí va el MURO!!!</Text>
+                {this.state.posts.map(post => <Text >{post.userName}</Text>)}
             </ScrollView>
         );
+    }
+
+    getPosts = () => {
+        axios.get('https://siqpik.herokuapp.com/api/posts')
+            .then(resp => resp.data)
+            .then(json => json.map(post => new Post(post)))
+            .then(posts => this.setState({posts}))
+            .catch(error => alert(error))
     }
 }
