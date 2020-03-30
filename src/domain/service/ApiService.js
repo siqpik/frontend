ba119d43e8e7146e6fc38e9e57379b5f9009e49f
ronn <ronnie@nextdigital.es>
2020-03-30 11:46:19 +0200

    Hanle api errors

--- src/domain/service/ApiService.js
+++ src/domain/service/ApiService.js
@@ -11,21 +11,24 @@ export const getJson = url => authenticatedRequest(url, 'GET')
     .then(response => response.json());
 
 export const authenticatedRequest = (url, method, body, contentType) => AsyncStorage.getItem(TOKEN_SESSION)
-    .then(token => genericFetch(url, method, {
-            'Authorization': 'Bearer ' + token,
-            'Content-Type': contentType != null ? contentType : 'application/json'
+    .then(token => genericFetch(url, method,
+        {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': contentType != null ? contentType : 'application/json'
         },
         body
-        )
-    )
+        ))
 
 export const genericFetch = (url, method, headers, body) => fetch(API_URL + url, {
     method: method,
     headers: headers,
     body: body
-}).then(response => {
+}).then(response => handleErrors(response))
+
+function handleErrors(response) {
     if (!response.ok) {
-        alert('Something went wrong: ' + JSON.stringify(response))
+        alert("Something went wrong: " + JSON.stringify(response))
+        throw Error(response.status);
     }
     return response;
-})
+}
---
+++ src/domain/shared/HeaderLogo.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Image, Text, View} from 'react-native';
+import {styles} from './style/styles';
+
+
+export class Logo extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image
+                    style={{ width: 100, height: 100 }}
+                    source={require('../../../src/assets/images/logo.jpg')}
+                />
+                <Text style={styles.logoText}>Siqpik</Text>
+            </View>
+        )
+    }
+}
