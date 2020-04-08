bb71184c8ca95d9158306d4afdd892175546ea4f
ronn <ronnie@nextdigital.es>
2020-04-08 11:48:57 +0200

    Create generic post method

--- src/domain/service/AuthenticationService.js
+++ src/domain/service/AuthenticationService.js
@@ -1,25 +1,13 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import AuthenticationRequest from '../model/AuthenticationRequest';
-import {API_URL, authenticatedRequest, HOST_URL} from './ApiService';
+import {API_URL, authenticatedRequest, genericPost, HOST_URL} from './ApiService';
 
 export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'USER_NAME_SESSION_ATTRIBUTE_NAME';
 export const TOKEN_SESSION = 'TOKEN_SESSION_ATTRIBUTE_NAME';
 
-const AUTH_URL = HOST_URL + '/authenticate';
-
-export const authenticate = (username, password) => fetch(AUTH_URL, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(new AuthenticationRequest(username, password))
-}).then(response => {
-    if (!response.ok){
-       throw new Error("Error trying to athenticate: " + response);
-    }
-
-    return response.json()
-}).then(json => registerSuccessfulLogin(json.jwt, username))
+export const authenticate = (username, password) =>
+    genericPost('/authenticate', new AuthenticationRequest(username, password))
+        .then(json => registerSuccessfulLogin(json.jwt, username))
 
 export const registerSuccessfulLogin = (token, username) => {
     AsyncStorage.setItem(TOKEN_SESSION, token)
