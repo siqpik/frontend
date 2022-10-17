51e4c9e4e126130b826d7508f03e89db104e9724
Ronn <ronnie.canas@gruposantander.com>
2022-10-17 23:36:38 +0200

    base for 2022

--- src/domain/camera/CameraView.js
+++ src/domain/camera/CameraView.js
@@ -7,44 +7,45 @@ import {PicturePreview} from "./PicturePreview";
 import {Camera, useCameraDevices} from "react-native-vision-camera";
 import {useIsFocused} from "@react-navigation/core";
 
-const isFocused = useIsFocused()
-const devices = useCameraDevices()
-const device = devices.back
-const camera = useRef(null)
-const takePhotoOptions = {
-    qualityPrioritization: 'speed',
-    flash: 'off'
-};
+export const CameraView = props => {
+    const isFocused = useIsFocused()
+    const devices = useCameraDevices()
+    const device = devices.back
+    const camera = useRef(null)
 
-export const CameraView = props => (
-    <View style={styles.container}>
-        {!props.showImage &&
-        <Camera
-            ref={camera}
-            style={StyleSheet.absoluteFill}
-            device={device}
-            isActive={isFocused}
-            photo={true}
-        >
-            {({ camera }) => (
-                <CameraButtons
-                    camera={camera}
-                    toggleDeviceCamera={props.toggleDeviceCamera}
-                    takePicture={props.takePicture}
-                    toggleFlash={props.toggleFlash}
-                    flashMode={props.flashMode}
+    const cameraPermission = await Camera.getCameraPermissionStatus()
+
+    if
+    return (
+        <View style={styles.container}>
+            {!props.showImage &&
+                <Camera
+                    ref={camera}
+                    style={styles.capture}
+                    device={device}
+                    isActive={isFocused}
+                    photo={true}
+                >
+                    {({ camera }) => (
+                        <CameraButtons
+                            camera={camera}
+                            toggleDeviceCamera={props.toggleDeviceCamera}
+                            takePicture={props.takePicture}
+                            toggleFlash={props.toggleFlash}
+                            flashMode={props.flashMode}
+                        />
+                    )}
+                </Camera>}
+
+            {props.showImage && <Image source={{uri: props.imageUri}} style={styles.takenPic} />}
+
+            {props.showImage &&
+                <PicturePreview
+                    imageUri={props.imageUri}
+                    showCameraAgain={() => props.showCameraAgain()}
+                    savePic={() => props.savePic()}
                 />
-                )}
-        </Camera>}
+            }
 
-        {props.showImage && <Image source={{uri: props.imageUri}} style={styles.takenPic} />}
-
-        {props.showImage &&
-        <PicturePreview
-            imageUri={props.imageUri}
-            showCameraAgain={() => props.showCameraAgain()}
-            savePic={() => props.savePic()}
-        />
-        }
-
-    </View>)
+        </View>)
+}
