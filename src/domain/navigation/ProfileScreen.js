ad53b3a996197b521c2db934c55beb710e3d7955
ronn <ronnie-filth@hotmail.com>
2023-02-27 20:07:20 +0100

    Prevent showing same when switching user

--- src/domain/navigation/ProfileScreen.js
+++ src/domain/navigation/ProfileScreen.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import {createDrawerNavigator, DrawerContentScrollView, DrawerItem, DrawerItemList} from '@react-navigation/drawer';
+import {createDrawerNavigator, DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
 import {Profile} from '../profile/Profile';
 import {logout} from '../service/AuthenticationService';
+import {useAppDispatch} from "../../config/hooks";
+import {reset} from "../home/modules/feedSlice";
 
-const CustomDrawerContent = props => (
-    <DrawerContentScrollView keyboardShouldPersistTaps='always' {...props}>
+const CustomDrawerContent = props => {
+    const dispatch = useAppDispatch()
 
-      <DrawerItem label="Log out" onPress={() => logout().then(() =>{
-          props.navigation.popToTop();
-          props.navigation.navigate('Login')
-      })} />
-    </DrawerContentScrollView>
-)
+    return (
+        <DrawerContentScrollView keyboardShouldPersistTaps='always' {...props}>
+
+            <DrawerItem label="Log out" onPress={() => logout().then(() => {
+                dispatch(reset())
+                props.navigation.popToTop();
+                props.navigation.navigate('Login')
+            })}/>
+        </DrawerContentScrollView>
+    )
+}
 
 const Drawer = createDrawerNavigator()
 
 export default () => (
-  <Drawer.Navigator drawerContent={props => CustomDrawerContent(props)} options={{ title: 'My home' }} screenOptions={{
-      headerShown: false,
-      drawerPosition: "right",
-      backgroundColor: '#efc6d4',
-      width: 40,
-  }}>
-    <Drawer.Screen name="Profile" component={Profile}/>
-  </Drawer.Navigator>
+    <Drawer.Navigator drawerContent={props => CustomDrawerContent(props)} options={{title: 'My home'}} screenOptions={{
+        headerShown: false,
+        drawerPosition: "right",
+        backgroundColor: '#efc6d4',
+        width: 40,
+    }}>
+        <Drawer.Screen name="Profile" component={Profile}/>
+    </Drawer.Navigator>
 )
