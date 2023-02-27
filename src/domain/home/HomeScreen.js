e73a5bec1427325a8a5b5fbb2efc1cf43b54f42f
ronn <ronnie-filth@hotmail.com>
2023-02-27 20:07:52 +0100

    Using redux to fetch feed

--- src/domain/home/HomeScreen.js
+++ src/domain/home/HomeScreen.js
@@ -1,38 +1,58 @@
 import React, {useEffect, useState} from 'react';
-import {BackHandler, RefreshControl, Text, TouchableOpacity} from 'react-native';
+import {RefreshControl} from 'react-native';
 import WallPost from './components/Post';
 import Post from './model/Post';
 import {deleteItem, getJson, post} from '../service/ApiService';
-import {useFocusEffect} from "@react-navigation/core";
 import {KeyboardAvoidingScrollView} from 'react-native-keyboard-avoiding-scroll-view';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {USER_NAME_SESSION_ATTRIBUTE_NAME} from "../service/AuthenticationService";
-import {connect} from "react-redux";
-import {getFeed} from "../../modules/reducer";
-import {bindActionCreators} from "redux";
-import {fetchFeed} from "./service/feed";
+import {
+    errorLikingPost,
+    errorSearchingFeed,
+    errorUnReactingPost,
+    likingPost,
+    reset,
+    searchingFeed,
+    successLikingPost,
+    successSearchingFeed,
+    successUnReactingPost,
+    unReactingToPost
+} from "./modules/feedSlice";
+import {useAppDispatch, useAppSelector} from "../../config/hooks";
 
-const HomeScreen = props => {
 
-    //const [posts, setPosts] = useState([]);
-    //const [postsPage, setPostsPage] = useState(1);
+export default props => {
+
+    const fetchFeed = () => {
+        dispatch(searchingFeed())
+        getJson(`/feed/${page}`)
+            .then(json => json.map(post => new Post(post)))
+            .then(newPosts => {
+                dispatch(successSearchingFeed(newPosts))
+            }).catch(error => {
+                console.log("error fetching feed: " + error)
+                dispatch(errorSearchingFeed())
+            }
+        )
+    }
+
+    const {posts, request, page} = useAppSelector(store => store.feed)
+    const dispatch = useAppDispatch()
+
     const [loggedUsername, setLoggedUsername] = useState([]);
     const [refreshing, setRefreshing] = React.useState(false);
 
-    const monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
-
     const wait = timeout => {
         return new Promise(resolve => {
-            props.fetchFeed;
+            fetchFeed()
             setTimeout(resolve, timeout);
         });
     }
 
     const onRefresh = React.useCallback(() => {
+        dispatch(reset())
         setRefreshing(true);
-        wait(1000).then(() => setRefreshing(false));
+        wait(100).then(() => setRefreshing(false));
     }, []);
 
     const {navigate} = props.navigation;
@@ -41,11 +61,11 @@ const HomeScreen = props => {
         AsyncStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME)
             .then(username => {
                 setLoggedUsername(username)
+                fetchFeed()
             })
-        fetchFeed();
     }, []);
 
-    useFocusEffect(
+    /*useFocusEffect(
         React.useCallback(() => {
             const onBackPress = () => BackHandler.exitApp()
 
@@ -54,63 +74,31 @@ const HomeScreen = props => {
             return () =>
                 BackHandler.removeEventListener('hardwareBackPress', onBackPress);
         }, [])
-    );
+    );*/
 
-    const fetchFeed = () => dispatch => {
-        dispatch(getFeed())
-        console.log("Ronn: " + props.postsPage)
-        getJson(`/feed/${props.postsPage}`)
-            .then(json => json.map(post => new Post(post)))
-            .then(nextPosts => {
-                setPosts([...posts, ...nextPosts])
-                setPostsPage(postsPage + 1)
-            })
-            .catch(error => {
-                if (error.message === '403') {
-                    props.navigation.navigate('Login')
-                }
-            })
-    }
 
     const togglePostReaction = (postId, toDelete) => {
-        callApiToToggleReaction(toDelete, postId);
-
-        toggleReaction(postId, toDelete);
-    }
-
-    const callApiToToggleReaction = (toDelete, postId) => {
         if (toDelete) {
+            dispatch(unReactingToPost())
             deleteItem('/post/' + postId + '/reaction')
-                .then(response => {
-                    if (!response.ok) {
-                        alert('You can not un-react this post now')
-                    }
+                .then(() => {
+                    dispatch(successUnReactingPost(postId))
                 })
-                .catch(error => alert('Borrando: ' + error))
+                .catch(error => {
+                    dispatch(errorUnReactingPost())
+                    alert('You can not un-react this post now: ' + error)
+                })
         } else {
+            dispatch(likingPost())
             post('/post/' + postId + '/reaction')
-                .then(response => {
-                    if (!response.ok) {
-                        alert('You can not like this post now')
-                    }
+                .then(() => {
+                    dispatch(successLikingPost(postId))
                 })
-                .catch(error => alert('posteando: ' + error))
+                .catch(error => {
+                    dispatch(errorLikingPost())
+                    alert('You can not like this post now\': ' + error)
+                })
         }
-    }
-
-    const toggleReaction = (postId, toDelete) => {
-        setPosts(previous => {
-            const prev = [...previous]
-            const indexToChange = prev.findIndex(p => p.id === postId)
-
-            prev[indexToChange] = {
-                ...prev[indexToChange],
-                iReacted: !toDelete,
-                likesCount: prev[indexToChange].likesCount + (toDelete ? -1 : 1)
-            }
-
-            return prev
-        })
     }
 
     const commentPost = (pictureID, comment) =>
@@ -118,10 +106,13 @@ const HomeScreen = props => {
             .catch(error => alert(error))
 
     const getFormattedDate = date => {
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
         const d = new Date(date);
         const postYear = d.getUTCFullYear()
-        return monthNames[d.getMonth()] + ' ' + d.getDate() + ' ' + (postYear
-        === new Date().getUTCFullYear() ? '' : postYear)
+
+        return monthNames[d.getMonth()] + ' ' + d.getDate() + ' ' + (postYear === new Date().getUTCFullYear() ? '' : postYear)
     }
 
     const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
@@ -145,19 +136,19 @@ const HomeScreen = props => {
             }
         >
             {
-                (props.posts.length === 0 || props.request !== Request.SUCCESS) && <WallPost
+                posts.length === 0 && <WallPost
                     date={getFormattedDate(new Date())}
                     mediaUrl={'https://res.cloudinary.com/siqpik/image/upload/v1670515879/ibscji05tdziedxvfz7p.jpg'}
                     username={'Siqpik'}
                     profilePicUrl={'https://res.cloudinary.com/siqpik/image/upload/v1670515879/ibscji05tdziedxvfz7p.jpg'}
-                    likesCount={999}
+                    likesCount={9999}
                     commentsCount={999}
                     comments={[]}
                     iReacted={true}
                     loggedUsername={loggedUsername}
                 />
             }
-            {props.posts.map((post, index) =>
+            {posts.map((post, index) =>
                 <WallPost
                     navigate={navigate}
                     id={post.id}
@@ -178,17 +169,3 @@ const HomeScreen = props => {
         </KeyboardAvoidingScrollView>
     );
 }
-
-const mapStateToProps = state => ({
-    posts: state.posts,
-    postsPage: state.postsPage,
-    request: state.request
-})
-
-const mapDispatchToProps = dispatch => bindActionCreators({
-    fetchFeed
-}, dispatch)
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(HomeScreen)
